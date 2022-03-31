c9b716add6656147da59117f1db626e149958bc7
Abdullah Al Akash <aabdullahalakash@gmail.com>
2022-03-31 23:11:56 +0600

    Update

--- src/components/Friends/singleFriend.js
+++ src/components/Friends/singleFriend.js
@@ -3,6 +3,7 @@ import React from 'react';
 const singleFriend = ({ friend }) => {
         return (
                 <div>
+                        {/* Display Friend Name and User Name */}
                         <h3>{friend.name}</h3>
                         <button>{friend.username}</button>
                 </div>
